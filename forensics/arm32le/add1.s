c942f5cc455a21d15f2a83076a9d996a9df81e04
Matt Liggett <mml@pobox.com>
2020-07-24 19:18:54 +0100

    Move forensics around, add add1 in C.

---
+++ forensics/arm32le/add1.s
@@ -0,0 +1,29 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 2
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"add1.c"
+	.text
+	.align	2
+	.global	add1
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	add1, %function
+add1:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	add	r0, r0, #1
+	bx	lr
+	.size	add1, .-add1
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits
